1abf20a26dd5423680d3e65a921ab35408957008
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-03-13 10:51:18 +0100

    Create Operatori matematici.py

---
+++ Operatori matematici.py
@@ -0,0 +1,48 @@
+# Operatori di comparazione
+
+# > maggiore
+# < minore
+# == uguale
+# <= minore uguañe
+# != diverso
+# is se un elemento è nella stesa posizione di un altro oggetto
+# is not se un elemento non è nella stessa posiz di un altro oggetto
+# in se un elemento si trova in un determinato luogo
+# not in not se un elemento non si trova in un determinato luogo
+
+
+# Operatori bitwise
+
+# utili per specificare piu di una condizione 
+# quando dobbiamo estrarre dei dati da un oggetto come un dataset
+
+# &  or  and, considera tutte le dichiarazioni
+# |  or   or, considera almeno una considerazione
+# ^    xor
+# ~    bitwise not
+# >>   left shift
+# <<   right shift
+
+
+# Operatori di assegnazione
+# per attribuire un nome a un dato oggetto
+
+# = Operatore di assegnazione di base
+x = 4+5
+y = 6
+# += Aggiunge un elemento e assegna il risultato al nome
+x+= y # (x = x + y )
+
+# -= Sottrae un elemento e assegna il risultato al nome
+x-= y # (x = x - y )
+# /= Divide un elemento e assegna il risultao al nome
+x/= y # (x = x/y )
+# *= Moltiplica un elemento e assegna il risultato al nome
+x*= y # (x = x % y )
+# %= modulo e riassegnazione
+x%= y # (x = x % y, ossia ilresto della divisione )
+# **= Potenza e riassegnazione
+x**= y # (x = x ** y )
+# //= Floor divisione e riassegnazione
+x//= y # (x = x // y , dvisione arrotondata per difetto, quindi toglie solo la virgola)
+
