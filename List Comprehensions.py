9c0681f16f45cdf7604339c2f64c65cc4517b48f
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2022-06-14 17:15:12 +0200

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,13 @@
+
+# Le list complehension ci permettono di creare nuove liste in modo semplice
+
+# Esempio dove otteniamo lo stesso risultato prima con un ciclo for e poi con le list complrehension
+
+lista =[]
+
+
+
+
+
+
+
